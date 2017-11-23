a8739d5ba7ab3571dd67ffa33efef0c31ee298fd
Noé Gambini <noe.gambini@gmail.com>
2017-11-23 13:52:47 +0100

    Project setup - continued

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { ClarityModule } from 'clarity-angular';
 
 import { AppComponent } from './app.component';
-
+import { routes } from './app.routes';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    ClarityModule.forRoot(),
+    RouterModule.forRoot(routes)
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
